ee604bb5d347e952a2f83d6cef9ea4086e1b5f2d
Mauro Clemente <maurosys@gmail.com>
2021-11-07 16:50:51 -0300

    about

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,70 +1,89 @@
-import Header from "../components/Layout/HeaderFixed";
-import Footer from "../components/Layout/Footer";
-import Facility from "../components/shop-style-five/Facility";
+import Header   from '../components/Layout/HeaderFixed';
+import Footer   from '../components/Layout/Footer';
+import Facility from '../components/shop-style-five/Facility';
 
 const About = () => {
-  return (
-    <>
-      <Header />
+	return (
+		<>
+			<Header/>
 
-      <section className="about-area ptb-60">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-lg-6 col-md-12">
-              <div className="about-content">
-                <h2>Sobre Nossa Loja</h2>
-                <p>
-                  Atualmente no mercado da moda existem diversos fatores que
-                  causam insegurança dos consumidores nas compras virtuais de
-                  vestuário.
-                </p>
+			<section className="about-area ptb-60">
+				<div className="container">
+					<div className="row align-items-center">
+						<div className="col-lg-12">
+							<div className="about-content">
+								<h2>RESERVE| EXPERIMENTE| COMPRE SOMENTE O QUE GOSTAR.</h2>
+								<div className="row">
+									<div className="col-lg-9">
+										<p>
+											Somos muito mais que uma loja on-line de Moda. Valorizar seu tempo é nossa principal Missão.
+										</p>
+										<p>
+											A ILooks nasceu com o propósito de inovar o mercado de Moda no Brasil oferecendo aos nossos clientes uma experiência de
+											compra única, totalmente diferente de qualquer loja on-line de Moda.
+										</p>
+										&nbsp;
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-lg-2 col-sm-12" style={{borderStyle: 'solid', borderRadius: 30, borderWidth: 1, padding: 10, margin: 5, minHeight: 300}}>
+										<div style={{textAlign: 'center', margin: 20}}>
+											<img src={require('../images/qsExperimente.jpg')}/>
+										</div>
+										<p>
+											<b>Experimente antes de comprar.</b><br/>
+											Um dos nossos diferenciais é oferecer aos nossos clientes a comodidade de experimentar,
+											tocar e sentir o produto antes da efetivação da compra.
+										</p>
+									</div>
 
-                <p>
-                  Fatores como não termos uma tabela de medidas padronizada no
-                  Brasil, processos para devoluções extremamente burocráticos e
-                  fretes com custos elevados.
-                </p>
+									<div className="col-lg-2 col-sm-12" style={{borderStyle: 'solid', borderRadius: 30, borderWidth: 1, padding: 10, margin: 5, minHeight: 300}}>
+										<div style={{textAlign: 'center', margin: 20}}>
+											<img src={require('../images/qsEntregas.jpg')}/>
+										</div>
+										<p>
+											<b>Entregas.</b><br/>
+											Entregas rápidas com experiência praticamente delivery também faz do nosso negócio único, afinal sempre aparece aquele evento,
+											ou compromisso de última hora em você pode contar com a agilidade de nossas entregas.
+										</p>
+									</div>
 
-                <p>
-                  A história da Ilook’s nasceu justamente com propósito de
-                  quebrar todos esses paradigmas e oferecer um serviço
-                  diferenciado aos nossos consumidores garantindo uma
-                  experiência de compra on-line 100% satisfatória
-                </p>
+									<div className="col-lg-2 col-sm-12" style={{borderStyle: 'solid', borderRadius: 30, borderWidth: 1, padding: 10, margin: 5, minHeight: 300}}>
+										<div style={{textAlign: 'center', margin: 20}}>
+											<img src={require('../images/qsDevolucao.jpg')}/>
+										</div>
+										<p>
+											<b>Devoluções.</b><br/>
+											Se nossas entregas vão até você, por que você precisa ir até os correios se algo der errado?
+											Entregamos e retiramos os produtos no conforto do seu lar oferecendo comodidade total ao nossos
+											consumidores sem que saiam de suas casas.
+										</p>
+									</div>
 
-                <p>
-                  Com entregas rápidas, processos de devoluções facilitados,
-                  comodidade de experimentar, tocar e sentir o produto antes da
-                  efetivação de compra, nossa missão é levar aos nossos clientes
-                  conforto e praticidade para o dia o dia.
-                </p>
-              </div>
-            </div>
+									<div className="col-lg-2 col-sm-12" style={{borderStyle: 'solid', borderRadius: 30, borderWidth: 1, padding: 10, margin: 5, minHeight: 300}}>
+										<div style={{textAlign: 'center', margin: 20}}>
+											<img src={require('../images/qsCuradoria.jpg')}/>
+										</div>
+										<p>
+											<b>Curadoria.</b><br/>
+											Nossa equipe conta com profissionais antenados no mundo da moda que selecionam as melhores marcas,
+											focando na curadoria para que a experiência do consumidor dentro da ILooks seja completa e facilitada.
+										</p>
+									</div>
+								</div>
 
-            <div className="col-lg-6 col-md-12">
-              <div className="about-image">
-                <img
-                  src={require("../images/about1.jpg")}
-                  className="about-img1"
-                  alt="image"
-                />
+							</div>
+						</div>
 
-                <img
-                  src={require("../images/about2.jpg")}
-                  className="about-img2"
-                  alt="image"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+					</div>
+				</div>
+			</section>
 
-      <Facility />
+			<Facility/>
 
-      <Footer />
-    </>
-  );
+			<Footer/>
+		</>
+	);
 };
 
 export default About;
