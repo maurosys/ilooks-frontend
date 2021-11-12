265ee3bb9df5381b89e5a86cd5b954be8af8b5ec
Mauro Clemente <maurosys@gmail.com>
2021-11-12 02:14:16 -0300

    correções para valor de outlet

--- store/ducks/Card/types.ts
+++ store/ducks/Card/types.ts
@@ -1,27 +1,28 @@
-import { ProductsDetails } from "../products/types";
+import {ProductsDetails} from '../products/types';
 
 /**
  * Actions Types
  */
 export enum CardTypes {
-  ADD_TO_CART = "@card/ADD_TO_CART",
-  REMOVE_ITEM = "@card/REMOVE_ITEM",
-  ALTER_QUANTITY = "@card/ALTER_QUANTITY",
-  GET_ALL = "@card/GET_ALL",
-  CLEAR_CART = "@card/CLEAR_CART",
+	ADD_TO_CART    = '@card/ADD_TO_CART',
+	REMOVE_ITEM    = '@card/REMOVE_ITEM',
+	ALTER_QUANTITY = '@card/ALTER_QUANTITY',
+	GET_ALL        = '@card/GET_ALL',
+	CLEAR_CART     = '@card/CLEAR_CART',
 }
 
 export interface card {
-  id: number;
-  title: string;
-  price: number;
-  image: string;
-  imageHover: string;
-  color?: string;
-  materialType?: string;
-  productDetail?: ProductsDetails;
-  qty: number;
-  total: number;
+	id: number;
+	title: string;
+	price: number;
+	outletPrice?: number;
+	image: string;
+	imageHover: string;
+	color?: string;
+	materialType?: string;
+	productDetail?: ProductsDetails;
+	qty: number;
+	total: number;
 }
 
 /**
@@ -29,9 +30,9 @@ export interface card {
  */
 
 export interface CardState {
-  readonly data: card[];
-  readonly loading: boolean;
-  readonly error: boolean;
-  total: number;
-  shipping: number;
+	readonly data: card[];
+	readonly loading: boolean;
+	readonly error: boolean;
+	total: number;
+	shipping: number;
 }
